43905c73175dd6f4b12cf12cba700ffdadb6b6f4
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-01-18 09:47:34 +0530

    data fetching resolved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -218,6 +218,94 @@ function Navbar() {
                 </div>
               </div>
             </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Country
+                <svg
+                  className="w-4 h-4 ml-1"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <div
+                id="dropdownNavbar"
+                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  className="py-1 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <Link
+                      to="/business"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Canada
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/entertainment"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      France
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/health"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Germany
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/science"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                       India
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/sports"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Japan
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/technology"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      United Kingdom
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/technology"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      United States
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
